1513c0f761928f44969ad17d39b2125aa7b04bdb
FraPochetti <fra.pochetti@gmail.com>
2021-01-10 16:58:40 +0100

    changing mkdir in imports to test lambda

--- icevision/utils/data_dir.py
+++ icevision/utils/data_dir.py
@@ -3,10 +3,10 @@ __all__ = ["get_data_dir", "get_root_dir"]
 from icevision.imports import *
 
 root_dir = Path.home() / ".icevision"
-root_dir.mkdir(exist_ok=True)
+root_dir.mkdir(exist_ok=True, parents=True)
 
 data_dir = root_dir / "data"
-data_dir.mkdir(exist_ok=True)
+data_dir.mkdir(exist_ok=True, parents=True)
 
 
 def get_data_dir():
